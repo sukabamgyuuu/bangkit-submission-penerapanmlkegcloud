8b6a38ffda8d02ed7aa5f76ec6b2eddaca7d2605
sukabamgyuuu <cacabesttt@gmail.com>
2024-12-04 10:42:43 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,16 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const dotenv = require('dotenv');
+const loadModel = require('../services/loadModel');
+const InputError = require('../exceptions/InputError');
 require('dotenv').config();
 
 
 dotenv.config();
-
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: '0.0.0.0',
+    host: 'localhost',
     routes: {
       payload: {
         maxBytes: 1000000,
@@ -19,12 +20,19 @@ const init = async () => {
       },
     },
   });
-
+  const model = await loadModel()
+  server.app.model = model
   server.route(routes);
-
-  server.ext('onPreResponse', (request, h) => {
-    const { response } = request;
-
+  server.ext('onPreResponse', function (request, h)  {
+    const response  = request.response;
+    if (response instanceof InputError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: `Terjadi kesalahan dalam melakukan prediksi`,
+      });
+      newResponse.code(400);
+      return newResponse;
+    }
     if (response.isBoom) {
       console.error('Unhandled Error:', response);
       return h.response({
@@ -32,17 +40,13 @@ const init = async () => {
         message: response.message || 'Internal Server Error',
       }).code(response.output.statusCode);
     }
-
     return h.continue;
   });
-
   await server.start();
   console.log(`Server running on ${server.info.uri}`);
 };
-
 process.on('unhandledRejection', (err) => {
   console.error('Unhandled Rejection:', err);
   process.exit(1);
 });
-
 init();
