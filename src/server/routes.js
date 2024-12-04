47ef6bf7bcb0e5c8d9517534363a581fb94244a0
sukabamgyuuu <cacabesttt@gmail.com>
2024-12-04 10:41:47 +0700

    Update routes.js

--- src/server/routes.js
+++ src/server/routes.js
@@ -7,8 +7,6 @@ const routes = [
       handler: postPredictHandler,
       options: {
         payload: {
-          output: 'stream', // Gunakan stream agar file dapat diterima
-          parse: true,
           multipart: true,
           allow: 'multipart/form-data',
           maxBytes: 1000000, // Ukuran maksimal file 1MB
